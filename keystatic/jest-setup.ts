adbdb2f741cb4963d8f446a841a5ba65ac2d9673
Emma Hamilton <git@emmas.town>
2023-02-14 11:48:38 +1000

    Move voussoir

--- keystatic/jest-setup.ts
+++ keystatic/jest-setup.ts
@@ -1,5 +1,7 @@
 import { TextDecoder, TextEncoder } from 'util';
+import { webcrypto } from 'crypto';
 
 // not sure why these aren't in jest's jsdom environment?
 globalThis.TextDecoder = TextDecoder as any;
 globalThis.TextEncoder = TextEncoder;
+globalThis.crypto = webcrypto as any;
