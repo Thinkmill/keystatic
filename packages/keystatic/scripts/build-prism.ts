a322dcb9d2a3bb8274a230bd7c412c2bdc334881
Emma Hamilton <git@emmas.town>
2023-02-21 14:55:39 +1000

    Remove next peer dep from @keystatic/core (#80)

---
+++ packages/keystatic/scripts/build-prism.ts
@@ -0,0 +1,82 @@
+import fs from 'fs/promises';
+
+const languages = [
+  'clike',
+  'c',
+  'cpp',
+  'arduino',
+  'bash',
+  'csharp',
+  'markup',
+  'css',
+  'diff',
+  'go',
+  'ini',
+  'java',
+  'regex',
+  'javascript',
+  'jsx',
+  'json',
+  'kotlin',
+  'less',
+  'lua',
+  'makefile',
+  'yaml',
+  'markdown',
+  'objectivec',
+  'perl',
+  'markup-templating',
+  'php',
+  'python',
+  'r',
+  'ruby',
+  'rust',
+  'sass',
+  'scss',
+  'sql',
+  'swift',
+  'typescript',
+  'tsx',
+  'basic',
+  'vbnet',
+];
+
+(async () => {
+  const otherLanguages = (
+    await Promise.all(
+      languages.map(lang =>
+        fs.readFile(require.resolve(`prismjs/components/prism-${lang}`), 'utf8')
+      )
+    )
+  ).join('\n\n');
+  const prism = (await fs.readFile(require.resolve('prismjs'), 'utf8'))
+    .replace(
+      `if (typeof module !== 'undefined' && module.exports) {
+\tmodule.exports = Prism;
+}
+
+// hack for components to work correctly in node.js
+if (typeof global !== 'undefined') {
+\tglobal.Prism = Prism;
+}`,
+      ''
+    )
+    .replace(
+      `var _self = (typeof window !== 'undefined')
+	? window   // if in browser
+	: (
+		(typeof WorkerGlobalScope !== 'undefined' && self instanceof WorkerGlobalScope)
+			? self // if in worker
+			: {}   // if in node js
+	);`,
+      `var _self = globalThis;`
+    );
+  await fs.writeFile(
+    `${__dirname}/../DocumentEditor/prism.js`,
+    '/* eslint-disable */\nglobalThis.Prism = { manual: true };\n' +
+      prism +
+      '\n' +
+      otherLanguages +
+      '\nexport default Prism;'
+  );
+})();
