6b26d133b4d6f8be623a7902ec8069b97a6ba52f
jossmac <jossmac@users.noreply.github.com>
2023-02-28 12:33:48 +1100

    demo app; more copy, less hot pink

--- keystatic/pages/index.tsx
+++ keystatic/pages/index.tsx
@@ -2,9 +2,26 @@ import Link from 'next/link';
 
 export default function Index() {
   return (
-    <div>
-      This page has some global css.{' '}
-      <Link href="/keystatic">Go to Keystatic</Link>
-    </div>
+    <main
+      style={{
+        alignItems: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        marginInline: 'auto',
+        maxWidth: 440,
+        minHeight: '100vh',
+      }}
+    >
+      <h1>Demo app</h1>
+      <p>
+        <a href="https://keystatic.thinkmill.com.au/">Keystatic</a> is a new
+        tool that makes Markdown, JSON and YAML content in your codebase
+        editable by humans.
+      </p>
+      <p>
+        <Link href="/keystatic">Start editing</Link>.
+      </p>
+    </main>
   );
 }
