cea2700b0d59e106a82a4369cf4894b96b59686b
Emma Hamilton <git@emmas.town>
2023-03-13 14:22:01 +1000

    Fix validation when a single form field is directly inside an array field

---
+++ .changeset/gorgeous-ladybugs-march.md
@@ -0,0 +1,5 @@
+---
+'@keystatic/core': patch
+---
+
+Fix validation when a single form field is directly inside an array field
--- packages/keystatic/DocumentEditor/component-blocks/utils.ts
+++ packages/keystatic/DocumentEditor/component-blocks/utils.ts
@@ -241,7 +241,7 @@ export function clientSideValidateProp(
       return true;
     }
     case 'form': {
-      if (path[path.length - 1] === slugField?.field) {
+      if (slugField && path[path.length - 1] === slugField?.field) {
         return schema.validate(value, {
           slugs: slugField.slugs,
         });
