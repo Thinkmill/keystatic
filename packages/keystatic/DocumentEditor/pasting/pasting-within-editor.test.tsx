ff240b59a2525ace95f59c2efcedd389e5b8d486
Emma Hamilton <git@emmas.town>
2023-04-06 10:06:05 +1000

    Fix pasting in some cases reversing the order of the pasted elements

---
+++ packages/keystatic/DocumentEditor/pasting/pasting-within-editor.test.tsx
@@ -0,0 +1,99 @@
+/** @jest-environment jsdom */
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { Transforms } from 'slate';
+import { makeEditor, jsx } from '../tests/utils';
+
+test('blockquote pasting', () => {
+  const editor = makeEditor(
+    <editor>
+      <blockquote>
+        <paragraph>
+          <text>
+            <anchor />a
+          </text>
+        </paragraph>
+      </blockquote>
+      <blockquote>
+        <paragraph>
+          <text>b</text>
+        </paragraph>
+      </blockquote>
+      <blockquote>
+        <paragraph>
+          <text>c</text>
+        </paragraph>
+      </blockquote>
+      <blockquote>
+        <paragraph>
+          <text>
+            d<focus />
+          </text>
+        </paragraph>
+      </blockquote>
+      <paragraph>
+        <text />
+      </paragraph>
+    </editor>
+  );
+  const fragment = editor.getFragment();
+  Transforms.collapse(editor, { edge: 'end' });
+  editor.insertFragment(fragment);
+  expect(editor).toMatchInlineSnapshot(`
+    <editor>
+      <blockquote>
+        <paragraph>
+          <text>
+            a
+          </text>
+        </paragraph>
+      </blockquote>
+      <blockquote>
+        <paragraph>
+          <text>
+            b
+          </text>
+        </paragraph>
+      </blockquote>
+      <blockquote>
+        <paragraph>
+          <text>
+            c
+          </text>
+        </paragraph>
+      </blockquote>
+      <blockquote>
+        <paragraph>
+          <text>
+            da
+          </text>
+        </paragraph>
+      </blockquote>
+      <blockquote>
+        <paragraph>
+          <text>
+            b
+          </text>
+        </paragraph>
+      </blockquote>
+      <blockquote>
+        <paragraph>
+          <text>
+            c
+          </text>
+        </paragraph>
+      </blockquote>
+      <blockquote>
+        <paragraph>
+          <text>
+            d
+            <cursor />
+          </text>
+        </paragraph>
+      </blockquote>
+      <paragraph>
+        <text />
+      </paragraph>
+    </editor>
+  `);
+});
