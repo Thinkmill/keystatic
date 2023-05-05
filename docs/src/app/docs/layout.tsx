52b4424885e89465b67b314f29ade990b192883e
Johan Brolin <johan.brolin@thinkmill.com.au>
2023-05-05 14:38:53 +1000

    Split out into layout

--- docs/src/app/docs/layout.tsx
+++ docs/src/app/docs/layout.tsx
@@ -1,3 +1,6 @@
+import Link from 'next/link';
+import Navigation from '../../components/navigation';
+import { NavContainer, NavList, NavItem } from '../../components/sidenav';
 import '../../styles/global.css';
 
 export const metadata = {
@@ -12,7 +15,58 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div>
+          {/** TOP NAV */}
+          <div className="border-b border-stone-400/20 flex items-center w-full fixed z-20">
+            <Navigation />
+          </div>
+
+          {/** MAIN */}
+          <main className="max-w-7xl min-h-screen mx-auto px-6">
+            {/** SIDE NAV */}
+            <NavContainer>
+              <NavList title="Getting started">
+                <NavItem label="Introduction" href="/" current />
+                <NavItem label="Automated setup" href="/" />
+                <NavItem label="Manual installation" href="/" />
+              </NavList>
+            </NavContainer>
+
+            {/** CONTENT */}
+            <div className="lg:pl-60 pt-24 flex-1">
+              <div className="py-10 pl-12 ">
+                {/** INNER CONTENT (markdoc goes here) */}
+                <div className="grid gap-6 grid-cols-[auto,12rem]">
+                  <div>{children}</div>
+
+                  <div className="sticky top-28 self-start">
+                    <h5 className="text-xs uppercase text-stone-600">
+                      On this page
+                    </h5>
+                  </div>
+                </div>
+
+                {/** FOOTER */}
+                <footer>
+                  <hr className="h-px my-8 border-stone-400/20 mb-8" />
+
+                  <p className="leading-none text-keystatic-gray-dark text-sm">
+                    &copy; {new Date().getFullYear()} Thinkmill. All rights
+                    reserved.{' '}
+                    <Link
+                      href="/privacy-policy"
+                      className="underline hover:text-black"
+                    >
+                      Privacy policy
+                    </Link>
+                  </p>
+                </footer>
+              </div>
+            </div>
+          </main>
+        </div>
+      </body>
     </html>
   );
 }
