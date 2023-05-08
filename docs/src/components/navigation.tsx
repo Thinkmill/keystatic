a4271a8a5cc987375d0ba0cc537b8eb66d2cabdd
Johan Brolin <johan.brolin@thinkmill.com.au>
2023-05-08 17:00:06 +1000

    Stroke

--- docs/src/components/navigation.tsx
+++ docs/src/components/navigation.tsx
@@ -60,7 +60,7 @@ export default function Index({ showCta = true }: NavigationProps) {
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                strokeWidth={1}
+                strokeWidth={1.5}
                 stroke="currentColor"
                 className="w-6 h-6"
               >
