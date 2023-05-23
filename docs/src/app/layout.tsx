a14bcedb75ecca4c4b4cbe79f62cf70f0508ec6d
Barnaby Bishop <barneyb@gmail.com>
2023-05-24 09:49:53 +1000

    public folder begone! - Removed fonts and used next/fonts to load inter - Use next's auto-metadata magic for app icons

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -1,7 +1,13 @@
-import Head from 'next/head';
+import { Inter } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/react';
 
 import '../styles/global.css';
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+});
 
 export const metadata = {
   title: 'Meet Keystatic',
@@ -33,30 +39,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
-      </Head>
-      <body>{children}</body>
+      <body className={inter.className}>{children}</body>
       <Analytics />
     </html>
   );
