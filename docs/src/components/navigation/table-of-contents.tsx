9ae5efdbadee80aad833d148dc8f678973041c49
Johan Brolin <johan.brolin@thinkmill.com.au>
2023-05-30 19:06:17 +1000

    Additions

--- docs/src/components/navigation/table-of-contents.tsx
+++ docs/src/components/navigation/table-of-contents.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import slugify from '@sindresorhus/slugify';
+import { useEffect, useRef, useState } from 'react';
 
 type Heading = {
   level: number;
@@ -10,16 +13,28 @@ type TableOfContentsProps = {
 };
 
 export function TableOfContents({ headings }: TableOfContentsProps) {
+  // TODO make fixed instead of position sticky
+  // TODO rewrite into a new object that you can map over
+  const slugs = headings.map(({ text }) => {
+    return `#${slugify(text)}`;
+  });
+
+  const { activeHeadingSlug } = useHeadingObserver(slugs);
+
   return (
     <div className="w-[12rem] sticky top-10 lg:top-32 self-start hidden md:block">
       <h2 className="text-xs uppercase text-neutral-500">On this page</h2>
 
       <ul className="mt-4 flex flex-col gap-3">
         {headings.map(({ level, text }) => (
-          <li>
+          <li key={text}>
             <a
-              className={`block text-sm text-neutral-600 leading-tight hover:underline ${
+              className={`block text-sm leading-tight hover:underline ${
                 level > 2 ? 'pl-2 text-xs' : ''
+              } ${
+                activeHeadingSlug === `${slugify(text)}`
+                  ? 'font-medium text-neutral-700'
+                  : 'text-neutral-600'
               }`}
               href={`#${slugify(text)}`}
             >
@@ -30,4 +45,33 @@ export function TableOfContents({ headings }: TableOfContentsProps) {
       </ul>
     </div>
   );
+}
+
+function useHeadingObserver(slugs: string[]) {
+  const observer = useRef<IntersectionObserver>();
+  const [activeHeadingSlug, setActiveHeadingSlug] = useState('');
+
+  const selectors = slugs.join(', ');
+
+  useEffect(() => {
+    const handleObserver = (entries: IntersectionObserverEntry[]) => {
+      entries.forEach(entry => {
+        if (entry?.isIntersecting) {
+          setActiveHeadingSlug(entry.target.id);
+        }
+      });
+    };
+
+    observer.current = new IntersectionObserver(handleObserver, {
+      rootMargin: '-96px 0px -50% 0px',
+    });
+
+    const elements = document.querySelectorAll(selectors);
+
+    elements.forEach(element => observer.current?.observe(element));
+
+    return () => observer.current?.disconnect();
+  }, [selectors]);
+
+  return { activeHeadingSlug };
 }
