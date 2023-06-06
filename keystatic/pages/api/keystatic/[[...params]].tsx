0b8b44c0155c8cab9928d74f535dc1963bef9b3e
Emma Hamilton <git@emmas.town>
2023-06-06 14:39:36 +1000

    Easier testing

--- keystatic/pages/api/keystatic/[[...params]].tsx
+++ keystatic/pages/api/keystatic/[[...params]].tsx
@@ -1,19 +1,7 @@
 import localConfig from '../../../local-config';
 import { makeAPIRouteHandler } from '@keystatic/next/api';
 
-function requiredEnv(name: string, val: string | undefined): string {
-  if (!val) {
-    throw new Error(`Missing required environment variable: ${name}`);
-  }
-  return val;
-}
-
 export default makeAPIRouteHandler({
-  secret: requiredEnv('NEXTAUTH_SECRET', process.env.NEXTAUTH_SECRET),
-  clientId: requiredEnv('GITHUB_CLIENT_ID', process.env.GITHUB_CLIENT_ID),
-  clientSecret: requiredEnv(
-    'GITHUB_CLIENT_SECRET',
-    process.env.GITHUB_CLIENT_SECRET
-  ),
   config: localConfig,
+  localBaseDirectory: './content',
 });
