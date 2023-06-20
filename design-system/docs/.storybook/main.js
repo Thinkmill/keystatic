b6a2fd9b3ef4c528125fe79e17c78a6bb201ede7
jossmac <2730833+jossmac@users.noreply.github.com>
2023-06-20 19:22:54 +1000

    prettier fix

--- design-system/docs/.storybook/main.js
+++ design-system/docs/.storybook/main.js
@@ -1,13 +1,29 @@
 const fs = require('fs');
 const path = require('path');
 module.exports = {
-  stories: [...fs.readdirSync(path.join(__dirname, '../../packages')).filter(dir => fs.existsSync(path.join(__dirname, `../../packages/${dir}/stories`))).map(f => `../../packages/${f}/stories/**/*.stories.tsx`), '../stories/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-a11y', '@storybook/addon-actions', '@storybook/addon-controls', '@storybook/addon-links', 'storybook-dark-mode', './addons/provider/register', './addons/theme/register'],
+  stories: [
+    ...fs
+      .readdirSync(path.join(__dirname, '../../packages'))
+      .filter(dir =>
+        fs.existsSync(path.join(__dirname, `../../packages/${dir}/stories`))
+      )
+      .map(f => `../../packages/${f}/stories/**/*.stories.tsx`),
+    '../stories/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: [
+    '@storybook/addon-a11y',
+    '@storybook/addon-actions',
+    '@storybook/addon-controls',
+    '@storybook/addon-links',
+    'storybook-dark-mode',
+    './addons/provider/register',
+    './addons/theme/register',
+  ],
   framework: {
     name: '@storybook/nextjs',
-    options: {}
+    options: {},
   },
   typescript: {
-    reactDocgen: false
-  }
+    reactDocgen: false,
+  },
 };
