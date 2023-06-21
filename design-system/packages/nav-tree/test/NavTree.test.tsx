b96b1dee4a3be4ede28b29a91b3242514a28689f
jossmac <2730833+jossmac@users.noreply.github.com>
2023-06-21 20:49:46 +1000

    fix test

--- design-system/packages/nav-tree/test/NavTree.test.tsx
+++ design-system/packages/nav-tree/test/NavTree.test.tsx
@@ -2,11 +2,15 @@ import '@testing-library/jest-dom';
 
 import { renderWithProvider } from '@voussoir/test-utils';
 
-import { NavTree } from '../src';
+import { Item, NavTree } from '../src';
 
 describe('nav-tree/NavTree', () => {
   it('renders', () => {
-    const { queryByText } = renderWithProvider(<NavTree>Test</NavTree>);
+    const { queryByText } = renderWithProvider(
+      <NavTree onAction={() => {}}>
+        <Item>Test</Item>
+      </NavTree>
+    );
 
     expect(queryByText('Test')).toBeTruthy();
   });
