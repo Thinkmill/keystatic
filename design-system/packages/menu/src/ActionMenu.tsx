a7bbbc6c4c5d7ad8d2c7dad2f02c2574ee504c11
jossmac <2730833+jossmac@users.noreply.github.com>
2023-06-28 11:38:52 +1000

    fix menu types

--- design-system/packages/menu/src/ActionMenu.tsx
+++ design-system/packages/menu/src/ActionMenu.tsx
@@ -38,14 +38,8 @@ function ActionMenu<T extends object>(
       <Menu
         children={props.children}
         items={props.items}
-        disabledBehavior={props.disabledBehavior}
         disabledKeys={props.disabledKeys}
-        disallowEmptySelection={props.disallowEmptySelection}
         onAction={props.onAction}
-        onSelectionChange={props.onSelectionChange}
-        selectionMode={props.selectionMode}
-        selectedKeys={props.selectedKeys}
-        defaultSelectedKeys={props.defaultSelectedKeys}
       />
     </MenuTrigger>
   );
