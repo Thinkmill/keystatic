9c867acb6f3bfddf06505815485e87d0633eb01a
Simon Vrachliotis <simon@simonswiss.com>
2023-06-30 09:21:53 +1000

    Add Astro template

---
+++ templates/astro/keystatic.config.tsx
@@ -0,0 +1,26 @@
+// keystatic.config.tsx
+import { config, fields, collection } from '@keystatic/core'
+
+export default config({
+  storage: {
+    kind: 'local',
+  },
+  collections: {
+    posts: collection({
+      label: 'Posts',
+      slugField: 'title',
+      path: 'src/content/posts/*',
+      format: { contentField: 'content' },
+      schema: {
+        title: fields.slug({ name: { label: 'Title' } }),
+        content: fields.document({
+          label: 'Content',
+          formatting: true,
+          dividers: true,
+          links: true,
+          images: true,
+        }),
+      },
+    }),
+  },
+})
