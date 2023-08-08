21ca4efef48497be78911ff7859cb725d2046d82
jossmac <2730833+jossmac@users.noreply.github.com>
2023-08-08 13:06:05 +1000

    add test for typographyToCapsize

---
+++ design-system/primitives/transformers/typographyToCapsize.test.ts
@@ -0,0 +1,54 @@
+import StyleDictionary from 'style-dictionary';
+
+import { typographyToCapsize } from './typographyToCapsize';
+import { getMockToken } from '../test-utilities';
+
+describe('Transformer: typographyToCapsize', () => {
+  const tokens = [
+    getMockToken({
+      value: '',
+      $type: 'color',
+    }),
+    getMockToken({
+      path: ['typography', 'baselineTrim'],
+      value: {
+        size: '14px',
+        lineheight: 1.4,
+      },
+      $type: 'capsize',
+    }),
+    getMockToken({
+      path: ['typography', 'capheightTrim'],
+      value: {
+        size: '14px',
+        lineheight: 1.4,
+      },
+      $type: 'capsize',
+    }),
+    getMockToken({
+      path: ['typography', 'capheight'],
+      value: {
+        size: '14px',
+        lineheight: 1.4,
+      },
+      $type: 'capsize',
+    }),
+    getMockToken({
+      value: '',
+    }),
+  ];
+
+  it('matches `capsize` tokens with required meta as value', () => {
+    expect(
+      tokens.filter(typographyToCapsize.matcher as StyleDictionary.Matcher)
+    ).toStrictEqual([tokens[1], tokens[2], tokens[3]]);
+  });
+
+  it('transforms `capsize` tokens', () => {
+    expect(
+      tokens
+        .filter(typographyToCapsize.matcher as StyleDictionary.Matcher)
+        .map(item => typographyToCapsize.transformer(item, {}))
+    ).toStrictEqual(['-0.3364em', '-0.3364em', '10.1818px']);
+  });
+});
