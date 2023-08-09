f600d1d9e1cc478915f3f757994095d10cdde539
Emma Hamilton <git@emmas.town>
2023-08-09 13:54:44 +1000

    Handle unexpected values in color scheme local storage

--- packages/keystatic/src/app/shell/theme.tsx
+++ packages/keystatic/src/app/shell/theme.tsx
@@ -14,23 +14,17 @@ const STORAGE_KEY = 'keystatic-color-scheme';
 
 // only for initializing the provider, for consumption use `useThemeContext()`
 export function useTheme() {
-  let initialValue = (localStorage.getItem(STORAGE_KEY) ||
-    'auto') as ColorScheme;
-
-  let [theme, setThemeValue] = useState<ColorScheme>(initialValue);
+  let [theme, setThemeValue] = useState<ColorScheme>(() => {
+    let storedValue = localStorage.getItem(STORAGE_KEY);
+    if (storedValue === 'light' || storedValue === 'dark') {
+      return storedValue;
+    }
+    return 'auto';
+  });
   let setTheme = (theme: ColorScheme) => {
     localStorage.setItem(STORAGE_KEY, theme);
     setThemeValue(theme);
   };
-
-  // fix for renamed value: "system" --> "auto"
-  // remove after a month or so: ~2023-10-01
-  useEffect(() => {
-    // @ts-expect-error
-    if (theme === 'system') {
-      setTheme('auto');
-    }
-  }, [theme]);
 
   return { theme, setTheme };
 }
