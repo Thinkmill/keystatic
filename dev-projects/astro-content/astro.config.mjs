c39d72d1a588b8768e8ce47e0825bf64daf6e644
Emma Hamilton <git@emmas.town>
2023-08-11 16:47:37 +1000

    progress on astro integration

--- dev-projects/astro-content/astro.config.mjs
+++ dev-projects/astro-content/astro.config.mjs
@@ -3,12 +3,13 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import markdoc from '@astrojs/markdoc';
+import keystatic from '@keystatic/astro';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
   site: 'https://example.com',
-  integrations: [tailwind(), react(), sitemap(), markdoc()],
+  integrations: [tailwind(), react(), sitemap(), markdoc(), keystatic()],
   vite: {
     ssr: {
       external: ['@keystatic/core'],
