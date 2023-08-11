e2a370c2e54801cc4331d2b67e242779e03c8902
Jed Watson <872310+JedWatson@users.noreply.github.com>
2023-08-11 15:06:55 +1000

    Adding blog and getting Astro example running

--- dev-projects/astro-content/astro.config.mjs
+++ dev-projects/astro-content/astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://example.com',
+  integrations: [tailwind(), sitemap()]
+});
