fe774f3c6680667c4ed4d7670f57f15644f596b5
Jed Watson <872310+JedWatson@users.noreply.github.com>
2023-08-11 15:57:51 +1000

    Adding keystatic config to new Astro content example

--- dev-projects/astro-content/astro.config.mjs
+++ dev-projects/astro-content/astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import markdoc from '@astrojs/markdoc';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'hybrid',
 	site: 'https://example.com',
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), react(), sitemap(), markdoc()]
 });
---
+++ dev-projects/astro-content/keystatic.page.ts
@@ -0,0 +1,4 @@
+import { makePage } from '@keystatic/astro/ui';
+import keystaticConfig from './keystatic.config';
+
+export const Keystatic = makePage(keystaticConfig);
