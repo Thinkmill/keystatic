140bd466fed0dfebc5c4397a16589b21139b629d
Simon Vrachliotis <simon@simonswiss.com>
2023-08-15 15:44:02 +1000

    updating astro template to use keystatic() integration

--- templates/astro/astro.config.mjs
+++ templates/astro/astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-
+import keystatic from '@keystatic/astro';
 import markdoc from '@astrojs/markdoc';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), markdoc()],
+  integrations: [react(), markdoc(), keystatic()],
   output: 'hybrid',
+  vite: {
+    ssr: {
+      external: ['@keystatic/core'],
+    },
+  },
 });
