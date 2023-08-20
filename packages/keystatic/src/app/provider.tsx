ca9d8289476f103adc46ca6271b124204c773013
Jed Watson <872310+JedWatson@users.noreply.github.com>
2023-08-20 21:48:18 +1000

    Add meta tag to prevent automatic zoom on mobile

--- packages/keystatic/src/app/provider.tsx
+++ packages/keystatic/src/app/provider.tsx
@@ -240,6 +240,10 @@ export default function Provider({
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
         <UrqlProvider value={useMemo(() => createUrqlClient(config), [config])}>
           {children}
         </UrqlProvider>
