90167752e092b4010566e1332c79ed90db27a1f4
Jed Watson <jed.watson@me.com>
2023-08-21 13:02:26 +1000

    Update packages/keystatic/src/app/provider.tsx
    Co-authored-by: Joss Mackison <2730833+jossmac@users.noreply.github.com>

--- packages/keystatic/src/app/provider.tsx
+++ packages/keystatic/src/app/provider.tsx
@@ -240,10 +240,7 @@ export default function Provider({
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
-        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <UrqlProvider value={useMemo(() => createUrqlClient(config), [config])}>
           {children}
         </UrqlProvider>
