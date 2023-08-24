be18ce6f41f1642291601d26071fd63704e3855c
Emma Hamilton <git@emmas.town>
2023-08-24 16:26:12 +1000

    Cloud image field (#583)

---
+++ packages/keystatic/src/form/fields/cloudImage/index.tsx
@@ -0,0 +1,49 @@
+import { cloudImageSchema } from '../../../component-blocks/cloud-image-schema';
+import { ObjectField } from '../../api';
+import { integer } from '../integer';
+import { object } from '../object';
+import { text } from '../text';
+import { CloudImageFieldInput } from './ui';
+
+export function cloudImage({
+  label,
+  description,
+  validation,
+}: {
+  label: string;
+  description?: string;
+  validation?: { isRequired?: boolean };
+}): ObjectField<typeof cloudImageSchema> {
+  return {
+    ...object(
+      {
+        src: text({
+          label: 'URL',
+          validation: {
+            length: {
+              min: validation?.isRequired ? 1 : 0,
+            },
+          },
+        }),
+        alt: text({
+          label: 'Alt text',
+        }),
+        height: integer({
+          label: 'Height',
+        }),
+        width: integer({
+          label: 'Width',
+        }),
+      },
+      { label, description }
+    ),
+    Input(props) {
+      return (
+        <CloudImageFieldInput
+          {...(props as any)}
+          isRequired={validation?.isRequired}
+        />
+      );
+    },
+  };
+}
