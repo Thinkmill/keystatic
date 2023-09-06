6409654b664ec78b6f16614a6a5fe726ebc9cc9f
Emma Hamilton <git@emmas.town>
2023-09-06 10:00:22 +1000

    Fix remix dev project

--- dev-projects/remix/app/routes/api.keystatic.$.tsx
+++ dev-projects/remix/app/routes/api.keystatic.$.tsx
@@ -1,5 +1,7 @@
-import { LoaderFunction } from '@remix-run/node';
+import { ActionFunction, LoaderFunction } from '@remix-run/node';
 import { handleLoader } from '@keystatic/remix/api';
 import config from '../../keystatic.config';
 
 export const loader: LoaderFunction = args => handleLoader({ config }, args);
+
+export const action: ActionFunction = args => handleLoader({ config }, args);
