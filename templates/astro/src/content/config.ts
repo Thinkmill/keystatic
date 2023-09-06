c41876f625f1e5309c87e6576b13430704d4c9a5
valery sizikova <93496731+valery-tm@users.noreply.github.com>
2023-09-06 18:23:22 +1000

    Astro config.ts in the template (#624)

---
+++ templates/astro/src/content/config.ts
@@ -0,0 +1,11 @@
+// @ts-ignore
+import { defineCollection, z } from 'astro:content';
+
+const posts = defineCollection({
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+  }),
+});
+
+export const collections = { posts };
