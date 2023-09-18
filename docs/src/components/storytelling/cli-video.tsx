677b76e56d874a5056f2ab9640bd49bd58d116cc
Chris Dale <131427388+chrisdale-io@users.noreply.github.com>
2023-09-18 18:04:21 +1000

    Update responsive layout

--- docs/src/components/storytelling/cli-video.tsx
+++ docs/src/components/storytelling/cli-video.tsx
@@ -1,8 +1,8 @@
 export default function CliVideo() {
   return (
     <section className="relative bg-white">
-      <div className="mx-auto grid max-w-5xl grid-cols-12 gap-6 px-6 py-12 sm:pb-12">
-        <div className="col-start-1 col-end-5 py-8">
+      <div className="mx-auto grid max-w-5xl grid-cols-8 gap-6 gap-y-10 px-6 py-12 md:grid-cols-12">
+        <div className="col-span-8 text-center sm:col-span-4 sm:col-start-3 md:col-start-1 md:col-end-6 md:mt-8 md:pr-6 md:text-left">
           <h2 className="text-2xl font-medium">
             Two-way editing, effortlessly with the Keystatic CLI
           </h2>
@@ -11,7 +11,7 @@ export default function CliVideo() {
             CLI
           </p>
         </div>
-        <div className="col-span-7 col-start-6">
+        <div className="col-span-8 sm:col-span-6 sm:col-start-2 md:col-span-7 md:col-start-6">
           <iframe
             className="aspect-video w-full max-w-full rounded-lg shadow"
             src="https://www.youtube.com/embed/E65Fx9all04?controls=0"
