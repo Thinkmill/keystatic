291b8592870e96b623901e8828c716f1cc4b8f3d
Arnav K <arnavkaushal09@gmail.com>
2023-10-02 19:20:15 +0530

    Update validateUrl.tsx

--- packages/keystatic/src/form/fields/url/validateUrl.tsx
+++ packages/keystatic/src/form/fields/url/validateUrl.tsx
@@ -3,13 +3,16 @@ import { isValidURL } from '../document/DocumentEditor/isValidURL';
 export function validateUrl(
   validation: { isRequired?: boolean } | undefined,
   value: unknown,
-  label: string
+  label: string,
 ) {
-  if (value !== null && (typeof value !== 'string' || !isValidURL(value))) {
-    return `${label} is not a valid URL`;
-  }
+  if (!(validation?.isRequired == false && value == "")) {
+    if (value !== null && (typeof value !== "string" || !isValidURL(value))) {
+      return `${label} is not a valid URL`;
+    }
 
-  if (validation?.isRequired && value === null) {
-    return `${label} is required`;
+    if (validation?.isRequired && value === null) {
+      return `${label} is required`;
+    }
   }
 }
+
