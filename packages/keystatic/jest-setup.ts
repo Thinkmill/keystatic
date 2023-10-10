4a6fc379c5f82566775cea9c3ec78a5de815552e
Emma Hamilton <git@emmas.town>
2023-10-10 16:45:05 +1000

    Allow using explicit resource management proposal

--- packages/keystatic/jest-setup.ts
+++ packages/keystatic/jest-setup.ts
@@ -5,3 +5,26 @@ import { TextDecoder, TextEncoder } from 'util';
 // not sure why these aren't in jest's jsdom environment?
 globalThis.TextDecoder = TextDecoder as any;
 globalThis.TextEncoder = TextEncoder;
+
+// remove these when Symbol.dispose/asyncDispose is in Node versions that we're using
+if (typeof Symbol.dispose !== 'symbol') {
+  Object.defineProperty(Symbol, 'dispose', {
+    // @ts-ignore
+    __proto__: null,
+    configurable: false,
+    enumerable: false,
+    value: Symbol('dispose'),
+    writable: false,
+  });
+}
+
+if (typeof Symbol.asyncDispose !== 'symbol') {
+  Object.defineProperty(Symbol, 'asyncDispose', {
+    // @ts-ignore
+    __proto__: null,
+    configurable: false,
+    enumerable: false,
+    value: Symbol('asyncDispose'),
+    writable: false,
+  });
+}
