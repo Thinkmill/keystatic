6c2e0fcd3271e65b516c7d079aec57efcf498607
Emma Hamilton <git@emmas.town>
2023-10-25 15:30:05 +1000

    Fix spelling of Referer

--- docs/src/app/preview/end/route.tsx
+++ docs/src/app/preview/end/route.tsx
@@ -5,9 +5,9 @@ export function POST(req: Request) {
   if (req.headers.get('origin') !== new URL(req.url).origin) {
     return new Response('Invalid origin', { status: 400 });
   }
-  const referrer = req.headers.get('Referrer');
+  const referrer = req.headers.get('Referer');
   if (!referrer) {
-    return new Response('Missing referrer', { status: 400 });
+    return new Response('Missing Referer', { status: 400 });
   }
   draftMode().disable();
   cookies().delete('ks-branch');
