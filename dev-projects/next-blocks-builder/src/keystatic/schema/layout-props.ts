9dac4f371f1d2f58e8ab8009797027e5895ba700
Simon Vrachliotis <simon@simonswiss.com>
2023-10-26 10:11:17 +1100

    layout props types generated from options

--- dev-projects/next-blocks-builder/src/keystatic/schema/layout-props.ts
+++ dev-projects/next-blocks-builder/src/keystatic/schema/layout-props.ts
@@ -1,8 +1,30 @@
 import { fields } from '@keystatic/core';
 
-export type Surface = 'white' | 'off-white' | 'black' | 'off-black' | 'splash';
-export type Padding = 'large' | 'medium' | 'small' | 'none';
-export type ContainerWidth = 'full' | 'large' | 'medium' | 'narrow';
+const surfaceOptions = [
+  { label: 'White', value: 'white' },
+  { label: 'Off-White', value: 'off-white' },
+  { label: 'Black', value: 'black' },
+  { label: 'Off-Black', value: 'off-black' },
+  { label: 'Splash', value: 'splash' },
+];
+
+const paddingOptions = [
+  { label: 'Large', value: 'large' },
+  { label: 'Medium', value: 'medium' },
+  { label: 'Small', value: 'small' },
+  { label: 'None', value: 'none' },
+];
+
+const containerWidthOptions = [
+  { label: 'Full', value: 'full' },
+  { label: 'Large', value: 'large' },
+  { label: 'Medium', value: 'medium' },
+  { label: 'Narrow', value: 'narrow' },
+];
+
+export type Surface = (typeof surfaceOptions)[number]['value'];
+export type Padding = (typeof paddingOptions)[number]['value'];
+export type ContainerWidth = (typeof containerWidthOptions)[number]['value'];
 
 export type LayoutProps = {
   surface: Surface;
@@ -16,46 +38,25 @@ export type LayoutProps = {
 // ----------------------------------
 export const surface = fields.select({
   label: 'Surface (background color)',
-  options: [
-    { label: 'White', value: 'white' },
-    { label: 'Off-White', value: 'off-white' },
-    { label: 'Black', value: 'black' },
-    { label: 'Off-Black', value: 'off-black' },
-    { label: 'Splash', value: 'splash' },
-  ],
+  options: surfaceOptions,
   defaultValue: 'white',
 });
 
 export const paddingTop = fields.select({
   label: 'Padding top',
-  options: [
-    { label: 'Large', value: 'large' },
-    { label: 'Medium', value: 'medium' },
-    { label: 'Small', value: 'small' },
-    { label: 'None', value: 'none' },
-  ],
+  options: paddingOptions,
   defaultValue: 'medium',
 });
 
 export const paddingBottom = fields.select({
   label: 'Padding bottom',
-  options: [
-    { label: 'Large', value: 'large' },
-    { label: 'Medium', value: 'medium' },
-    { label: 'Small', value: 'small' },
-    { label: 'None', value: 'none' },
-  ],
+  options: paddingOptions,
   defaultValue: 'medium',
 });
 
 export const containerWidth = fields.select({
   label: 'Container width',
-  options: [
-    { label: 'Full', value: 'full' },
-    { label: 'Large', value: 'large' },
-    { label: 'Medium', value: 'medium' },
-    { label: 'Narrow', value: 'narrow' },
-  ],
+  options: containerWidthOptions,
   defaultValue: 'medium',
 });
 
