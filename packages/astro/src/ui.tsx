c4ebd05613f29b2bf203fd89437f67bbbca65256
Emma Hamilton <git@emmas.town>
2023-11-02 11:43:12 +1000

    Use minimal router in keystatic core over different ones for each framework

--- packages/astro/src/ui.tsx
+++ packages/astro/src/ui.tsx
@@ -1,82 +1,14 @@
-import React, {
-  AnchorHTMLAttributes,
-  forwardRef,
-  Ref,
-  useContext,
-  useMemo,
-} from 'react';
+import React from 'react';
 import { Config } from '@keystatic/core';
-import { Keystatic as GenericKeystatic, Router } from '@keystatic/core/ui';
-import {
-  createBrowserRouter,
-  Link,
-  RouterProvider,
-  useHref,
-  useLocation,
-  useNavigate,
-} from 'react-router-dom';
-
-const KeystaticLink = forwardRef(function KeystaticLink(
-  {
-    href,
-    ...props
-  }: { href: string } & AnchorHTMLAttributes<HTMLAnchorElement>,
-  ref: Ref<HTMLAnchorElement>
-) {
-  return <Link to={href} {...props} ref={ref} />;
-});
-
-function ReactRouterKeystatic() {
-  const config = useContext(ConfigContext)!;
-  const navigate = useNavigate();
-  const location = useLocation();
-  const href = useHref(location);
-  const keystaticRouter = useMemo((): Router => {
-    const replaced = location.pathname.replace(/^\/keystatic\/?/, '');
-    const params =
-      replaced === '' ? [] : replaced.split('/').map(decodeURIComponent);
-
-    return {
-      push(path) {
-        navigate(path);
-      },
-      replace(path) {
-        navigate(path, { replace: true });
-      },
-      href,
-      params,
-    };
-  }, [navigate, href, location.pathname]);
-  return (
-    <GenericKeystatic
-      router={keystaticRouter}
-      config={config}
-      link={KeystaticLink}
-      appSlug={appSlug}
-    />
-  );
-}
+import { Keystatic as GenericKeystatic } from '@keystatic/core/ui';
 
 const appSlug = {
   envName: 'PUBLIC_KEYSTATIC_GITHUB_APP_SLUG',
   value: import.meta.env.PUBLIC_KEYSTATIC_GITHUB_APP_SLUG,
 };
 
-const ConfigContext = React.createContext<Config<any, any> | null>(null);
-
-const router = createBrowserRouter([
-  {
-    path: '/keystatic/*',
-    element: <ReactRouterKeystatic />,
-  },
-]);
-
 export function makePage(config: Config<any, any>) {
   return function Keystatic() {
-    return (
-      <ConfigContext.Provider value={config}>
-        <RouterProvider router={router} />
-      </ConfigContext.Provider>
-    );
+    return <GenericKeystatic config={config} appSlug={appSlug} />;
   };
 }
--- packages/keystatic/src/app/router.tsx
+++ packages/keystatic/src/app/router.tsx
@@ -1,4 +1,12 @@
-import { createContext, ReactNode, useContext } from 'react';
+import React, {
+  createContext,
+  ReactNode,
+  startTransition,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react';
 
 export type Router = {
   push: (path: string) => void;
@@ -9,9 +17,52 @@ export type Router = {
 
 const RouterContext = createContext<Router | null>(null);
 
-export function RouterProvider(props: { router: Router; children: ReactNode }) {
+export function RouterProvider(props: { children: ReactNode }) {
+  const [url, setUrl] = useState(() => window.location.href);
+
+  const router = useMemo((): Router => {
+    function navigate(url: string, replace: boolean) {
+      const newUrl = new URL(url, window.location.href);
+      if (
+        newUrl.origin !== window.location.origin ||
+        !newUrl.pathname.startsWith('/keystatic')
+      ) {
+        window.location.assign(newUrl);
+        return;
+      }
+      window.history[replace ? 'replaceState' : 'pushState'](null, '', newUrl);
+      startTransition(() => {
+        setUrl(newUrl.toString());
+      });
+    }
+    const replaced = location.pathname.replace(/^\/keystatic\/?/, '');
+    const params =
+      replaced === '' ? [] : replaced.split('/').map(decodeURIComponent);
+    const parsedUrl = new URL(url);
+    return {
+      href: parsedUrl.pathname + parsedUrl.search,
+      replace(path) {
+        navigate(path, true);
+      },
+      push(path) {
+        navigate(path, false);
+      },
+      params,
+    };
+  }, [url]);
+  useEffect(() => {
+    const handleNavigate = () => {
+      startTransition(() => {
+        setUrl(window.location.href);
+      });
+    };
+    window.addEventListener('popstate', handleNavigate);
+    return () => {
+      window.removeEventListener('popstate', handleNavigate);
+    };
+  }, []);
   return (
-    <RouterContext.Provider value={props.router}>
+    <RouterContext.Provider value={router}>
       {props.children}
     </RouterContext.Provider>
   );
