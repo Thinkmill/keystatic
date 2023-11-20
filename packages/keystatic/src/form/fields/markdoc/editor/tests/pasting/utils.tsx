237a6f9c18385750d4b01096bbf9b49b4bc2e5bb
Emma Hamilton <git@emmas.town>
2023-11-20 11:33:15 +1000

    Pasting HTML tests

---
+++ packages/keystatic/src/form/fields/markdoc/editor/tests/pasting/utils.tsx
@@ -0,0 +1,18 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { renderEditor, jsx } from '../utils';
+import { MyDataTransfer } from './data-transfer';
+
+export async function htmlToEditor(html: string) {
+  const { state, user } = renderEditor(
+    <doc>
+      <paragraph>
+        <cursor />
+      </paragraph>
+    </doc>
+  );
+  const dataTransfer = new MyDataTransfer();
+  dataTransfer.setData('text/html', html);
+  await user.paste(dataTransfer);
+  return state();
+}
