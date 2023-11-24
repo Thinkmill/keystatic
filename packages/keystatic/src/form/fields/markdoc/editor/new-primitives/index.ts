60d6ae8c1a33437f2c4bbb11175013fdf290ac49
Emma Hamilton <git@emmas.town>
2023-11-24 14:33:56 +1000

    Move listbox

--- packages/keystatic/src/form/fields/markdoc/editor/new-primitives/index.ts
+++
@@ -1,7 +0,0 @@
-export {
-  EditorListbox,
-  useEditorListbox,
-  type EditorListboxProps,
-} from './EditorListbox';
-export type { ReferenceElement, VirtualElement } from '@floating-ui/react';
-export { Item, Section } from '@keystar/ui/listbox';
