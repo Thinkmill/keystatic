abdd3761f4b09d5fa7b9a5afb001c46b2728a896
Simon Vrachliotis <simon@simonswiss.com>
2024-03-11 16:48:37 +1100

    CLI video copy on homepage

--- docs/src/components/storytelling/cli-video.tsx
+++ docs/src/components/storytelling/cli-video.tsx
@@ -4,11 +4,15 @@ export default function CliVideo() {
       <div className="mx-auto grid max-w-5xl grid-cols-8 gap-6 gap-y-10 px-6 py-12 md:grid-cols-12">
         <div className="col-span-8 text-center sm:col-span-6 sm:col-start-2 md:col-start-1 md:col-end-6 md:mt-8 md:pr-6 md:text-left">
           <h2 className="mb-4 text-2xl font-medium">
-            Two-way editing, effortlessly with the Keystatic CLI
+            Two-way editing, effortlessly.
           </h2>
+          <p className="mb-4">
+            Create a new Next.js, Astro or Remix project in seconds with the
+            Keystatic CLI.
+          </p>
           <p>
-            Create a new Next.js or Astro project in seconds with the Keystatic
-            CLI.
+            Edit content from an the Admin UI, or directly in your codebase
+            files.
           </p>
         </div>
         <div className="col-span-8 sm:col-span-6 sm:col-start-2 md:col-span-7 md:col-start-6">
