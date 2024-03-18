331b43d1e8a60343f7ce8691e9a479a7879e657a
Emma Hamilton <git@emmas.town>
2024-03-18 10:39:10 +1000

    Fix very slow editing when replacing one large file with another large file of the same size

--- packages/keystatic/src/app/useHasChanged.ts
+++ packages/keystatic/src/app/useHasChanged.ts
@@ -1,8 +1,10 @@
 import isEqual from 'fast-deep-equal';
-import { useCallback, useMemo } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import { ComponentSchema, ObjectField } from '../form/api';
 import { getSlugFromState } from './utils';
 import { serializeProps } from '../form/serialize-props';
+import { useData } from './useData';
+import { blobSha } from './trees';
 
 export function useHasChanged(args: {
   initialState: unknown;
@@ -11,31 +13,62 @@ export function useHasChanged(args: {
   slugField: string | undefined;
 }) {
   const serialize = useCallback(
-    (state: unknown) => {
+    async (state: unknown) => {
       const slug = args.slugField
         ? getSlugFromState(
             { schema: args.schema.fields, slugField: args.slugField },
             state as Record<string, unknown>
           )
         : undefined;
+      const serializedState = serializeProps(
+        state,
+        args.schema,
+        args.slugField,
+        slug,
+        true
+      );
       return {
         slug,
-        state: serializeProps(state, args.schema, args.slugField, slug, true),
+        value: serializedState.value,
+        extraFiles: Object.fromEntries(
+          await Promise.all(
+            serializedState.extraFiles.map(async val => [
+              JSON.stringify([val.path, val.parent]),
+              await blobSha(val.contents),
+            ])
+          )
+        ),
       };
     },
     [args.schema, args.slugField]
   );
-  const initialFilesForUpdate = useMemo(
-    () => (args.initialState === null ? null : serialize(args.initialState)),
-    [args.initialState, serialize]
+  const initialFilesForUpdate = useData(
+    useCallback(
+      () => (args.initialState === null ? null : serialize(args.initialState)),
+      [args.initialState, serialize]
+    )
   );
 
-  const filesForUpdate = useMemo(
-    () => serialize(args.state),
-    [serialize, args.state]
+  const filesForUpdate = useData(
+    useCallback(() => serialize(args.state), [serialize, args.state])
   );
 
-  return useMemo(() => {
-    return !isEqual(initialFilesForUpdate, filesForUpdate);
+  const hasChangedState = useMemo(() => {
+    if (
+      initialFilesForUpdate.kind === 'loaded' &&
+      filesForUpdate.kind === 'loaded'
+    ) {
+      const a = initialFilesForUpdate.data;
+      const b = filesForUpdate.data;
+      return !isEqual(a, b);
+    }
+    return 'unknown' as const;
   }, [initialFilesForUpdate, filesForUpdate]);
+
+  const [hasChanged, setHasChanged] = useState(false);
+
+  if (typeof hasChangedState === 'boolean' && hasChangedState !== hasChanged) {
+    setHasChanged(hasChangedState);
+  }
+  return hasChanged;
 }
