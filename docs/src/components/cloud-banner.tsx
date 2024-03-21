817f3ef0d76e9680d7fb087ce1f674c787153580
Simon Vrachliotis <simon@simonswiss.com>
2024-03-21 15:26:52 +1100

    Fix svg attribute casing + add aria-label

--- docs/src/components/cloud-banner.tsx
+++ docs/src/components/cloud-banner.tsx
@@ -8,7 +8,7 @@ export function CloudBanner() {
     >
       <div className="absolute inset-0 bg-pattern-texture opacity-50 transition duration-150 group-hover:opacity-90" />
       <span className="relative mx-auto block max-w-5xl px-6 py-6 md:py-10">
-        <span className="text-pretty flex items-center justify-center gap-4 text-center text-base text-white md:text-lg">
+        <span className="flex items-center justify-center gap-4 text-pretty text-center text-base text-white md:text-lg">
           <span>
             Simplify GitHub authentication, optimise images and more with{' '}
             <span className="underline">Keystatic Cloud</span>
@@ -17,14 +17,15 @@ export function CloudBanner() {
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 20 20"
             fill="currentColor"
-            className="size-6 hidden shrink-0 transition group-hover:translate-x-1 lg:block"
+            className="hidden size-6 shrink-0 transition group-hover:translate-x-1 lg:block"
             aria-hidden="true"
+            aria-label="Learn more about Keystatic Cloud"
           >
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M3 10a.75.75 0 01.75-.75h10.638L10.23 5.29a.75.75 0 111.04-1.08l5.5 5.25a.75.75 0 010 1.08l-5.5 5.25a.75.75 0 11-1.04-1.08l4.158-3.96H3.75A.75.75 0 013 10z"
-              clip-rule="evenodd"
-            ></path>
+              clipRule="evenodd"
+            />
           </svg>
         </span>
       </span>
