fde997e7b8dcb7218e61927b10e7d69d1f2302f6
jossmac <2730833+jossmac@users.noreply.github.com>
2024-04-08 11:18:27 +1000

    document utils

--- design-system/pkg/src/utils/useRenderProps.ts
+++ design-system/pkg/src/utils/useRenderProps.ts
@@ -1,9 +1,23 @@
 import { WithRenderProps } from '@keystar/ui/types';
+import { useMemo } from 'react';
 
-export function useRenderProps<T>(props: WithRenderProps<T>, values: T) {
-  if (typeof props.children === 'function') {
-    return props.children(values);
-  } else {
-    return props.children;
-  }
+/**
+ * Render the children of a component, either as a function or a ReactNode.
+ *
+ * @param props The props of the component.
+ * @param values A **memoized** object, which is passed as the argument to your `props.children` render fn.
+ */
+export function useRenderProps<T extends Record<string, any>>(
+  props: WithRenderProps<T>,
+  values: T
+) {
+  let { children } = props;
+
+  return useMemo(() => {
+    if (typeof children === 'function') {
+      return children(values);
+    }
+
+    return children;
+  }, [children, values]);
 }
