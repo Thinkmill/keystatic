eb5301dbc4de07655bc0bcf02dcbd8db163f7d30
jossmac <2730833+jossmac@users.noreply.github.com>
2024-04-19 09:51:57 +1000

    super draft `multiRelationship` field

---
+++ packages/keystatic/src/form/fields/multiRelationship/index.tsx
@@ -0,0 +1,57 @@
+import { BasicFormField } from '../../api';
+import { FieldDataError } from '../error';
+import {
+  RequiredValidation,
+  assertRequired,
+  basicFormFieldWithSimpleReaderParse,
+} from '../utils';
+import { MultiRelationshipInput } from '#field-ui/multiRelationship';
+
+export function multiRelationship<IsRequired extends boolean | undefined>({
+  label,
+  collection,
+  validation,
+  description,
+}: {
+  label: string;
+  collection: string;
+  validation?: { isRequired?: IsRequired };
+  description?: string;
+} & RequiredValidation<IsRequired>): BasicFormField<
+  string | null,
+  string | (IsRequired extends true ? never : null)
+> {
+  return basicFormFieldWithSimpleReaderParse({
+    label,
+    Input(props) {
+      return (
+        <MultiRelationshipInput
+          label={label}
+          collection={collection}
+          description={description}
+          validation={validation}
+          {...props}
+        />
+      );
+    },
+    defaultValue() {
+      return null;
+    },
+    parse(value) {
+      if (value === undefined) {
+        return null;
+      }
+      if (typeof value !== 'string') {
+        throw new FieldDataError('Must be a string');
+      }
+      return value;
+    },
+    validate(value) {
+      assertRequired(value, validation, label);
+      return value;
+    },
+    serialize(value) {
+      return { value: value === null ? undefined : value };
+    },
+  });
+}
---
+++ packages/keystatic/src/form/fields/multiRelationship/ui.tsx
@@ -0,0 +1,243 @@
+import { Item } from '@react-stately/collections';
+import { ItemDropTarget, Selection } from '@react-types/shared';
+import { useReducer, useMemo, useState, useEffect, Key } from 'react';
+
+import { ActionBar, ActionBarContainer } from '@keystar/ui/action-bar';
+import { Combobox } from '@keystar/ui/combobox';
+import { useDragAndDrop } from '@keystar/ui/drag-and-drop';
+import { Icon } from '@keystar/ui/icon';
+import { trash2Icon } from '@keystar/ui/icon/icons/trash2Icon';
+import { VStack } from '@keystar/ui/layout';
+import { ListView } from '@keystar/ui/list-view';
+import { css, tokenSchema } from '@keystar/ui/style';
+import { Text } from '@keystar/ui/typography';
+
+import { FormFieldInputProps } from '../../api';
+import { useSlugsInCollection } from '../../../app/useSlugsInCollection';
+import { ExtraFieldInputProps } from '../../form-from-preview';
+import { move } from '../../ui-utils';
+
+type TempElement = { key: string };
+
+export function MultiRelationshipInput(
+  props: FormFieldInputProps<string | null> & {
+    collection: string;
+    validation: { isRequired?: boolean } | undefined;
+    label: string;
+    description: string | undefined;
+  }
+) {
+  const [blurred, onBlur] = useReducer(() => true, false);
+  const slugs = useSlugsInCollection(props.collection);
+  const options = useMemo(() => {
+    return slugs.map(slug => ({ slug }));
+  }, [slugs]);
+
+  const _errorMessage =
+    (props.forceValidation || blurred) &&
+    props.validation?.isRequired &&
+    props.value === null
+      ? `${props.label} is required`
+      : undefined;
+  // this state & effect shouldn't really exist
+  // it's here because react-aria/stately calls onSelectionChange with null
+  // after selecting an item if we immediately remove the error message
+  // so we delay it with an effect
+  const [errorMessage, setErrorMessage] = useState(_errorMessage);
+  useEffect(() => {
+    setErrorMessage(_errorMessage);
+  }, [_errorMessage]);
+
+  // TEMP implementation
+  const [elements, setElements] = useState<TempElement[]>([]);
+  const items = options.filter(
+    option => !elements.some(element => element.key === option.slug)
+  );
+
+  return (
+    <VStack gap="medium" minWidth={0}>
+      <Combobox
+        label={props.label}
+        description={props.description}
+        selectedKey={null}
+        placeholder={items.length === 0 ? 'All selected' : undefined}
+        onSelectionChange={key => {
+          if (typeof key === 'string') {
+            setElements(prevElements => [...prevElements, { key }]);
+          }
+        }}
+        disabledKeys={['No more items…']}
+        onBlur={onBlur}
+        autoFocus={props.autoFocus}
+        defaultItems={items.length ? items : [{ slug: 'No more items…' }]}
+        isReadOnly={items.length === 0}
+        isRequired={props.validation?.isRequired}
+        errorMessage={errorMessage}
+        width="auto"
+      >
+        {item => <Item key={item.slug}>{item.slug}</Item>}
+      </Combobox>
+      {/* {elements.length ? ( */}
+      {true ? (
+        <MultiRelationshipListView
+          autoFocus={props.autoFocus}
+          forceValidation={props.forceValidation}
+          onChange={setElements}
+          elements={elements}
+          aria-label={props.label}
+        />
+      ) : null}
+    </VStack>
+  );
+}
+
+function MultiRelationshipListView(
+  props: ExtraFieldInputProps & {
+    'aria-label': string;
+    elements: { key: string }[];
+    onChange: (elements: { key: string }[]) => void;
+  }
+) {
+  let [selectedKeys, setSelectedKeys] = useState<Selection>(new Set([]));
+  let onMove = (keys: Key[], target: ItemDropTarget) => {
+    const targetIndex = props.elements.findIndex(x => x.key === target.key);
+    if (targetIndex === -1) return;
+    const allKeys = props.elements.map(x => ({ key: x.key }));
+    const indexToMoveTo =
+      target.dropPosition === 'before' ? targetIndex : targetIndex + 1;
+    const indices = keys.map(key => allKeys.findIndex(x => x.key === key));
+    props.onChange(move(allKeys, indices, indexToMoveTo));
+  };
+
+  const dragType = useMemo(() => Math.random().toString(36), []);
+
+  let { dragAndDropHooks } = useDragAndDrop({
+    getItems(keys) {
+      // Use a drag type so the items can only be reordered within this list
+      // and not dragged elsewhere.
+      return [...keys].map(key => {
+        key = JSON.stringify(key);
+        return {
+          [dragType]: key,
+          'text/plain': key,
+        };
+      });
+    },
+    getAllowedDropOperations() {
+      return ['move', 'cancel'];
+    },
+    async onDrop(e) {
+      if (e.target.type !== 'root' && e.target.dropPosition !== 'on') {
+        let keys = [];
+        for (let item of e.items) {
+          if (item.kind === 'text') {
+            let key;
+            if (item.types.has(dragType)) {
+              key = JSON.parse(await item.getText(dragType));
+              keys.push(key);
+            } else if (item.types.has('text/plain')) {
+              // Fallback for Chrome Android case: https://bugs.chromium.org/p/chromium/issues/detail?id=1293803
+              // Multiple drag items are contained in a single string so we need to split them out
+              key = await item.getText('text/plain');
+              keys = key.split('\n').map(val => val.replaceAll('"', ''));
+            }
+          }
+        }
+        onMove(keys, e.target);
+      }
+    },
+    getDropOperation(target) {
+      if (target.type === 'root' || target.dropPosition === 'on') {
+        return 'cancel';
+      }
+
+      return 'move';
+    },
+  });
+
+  return (
+    <ActionBarContainer maxHeight="scale.3400" minHeight="scale.1600">
+      <ListView
+        aria-label={props['aria-label']}
+        items={props.elements}
+        dragAndDropHooks={dragAndDropHooks}
+        selectionMode="multiple"
+        onSelectionChange={setSelectedKeys}
+        selectedKeys={selectedKeys}
+        renderEmptyState={arrayFieldEmptyState}
+        // density="compact"
+        UNSAFE_className={css({
+          borderRadius: tokenSchema.size.radius.regular,
+        })}
+      >
+        {item => {
+          const label = item.key;
+          return (
+            <Item key={item.key} textValue={label}>
+              <Text>{label}</Text>
+              {/* <TooltipTrigger placement="start">
+              <ActionButton
+                onPress={() => {
+                  onRemoveKey(item.key);
+                }}
+                // UNSAFE_style={{ marginBlock: `calc(${tokenSchema.size.space.regular} * -1)` }}
+              >
+                <Icon src={trash2Icon} />
+              </ActionButton>
+              <Tooltip>{stringFormatter.format('delete')}</Tooltip>
+            </TooltipTrigger> */}
+            </Item>
+          );
+        }}
+      </ListView>
+      <ActionBar
+        selectedItemCount={selectedKeys === 'all' ? 'all' : selectedKeys.size}
+        onClearSelection={() => setSelectedKeys(new Set())}
+        onAction={key => {
+          if (key === 'delete') {
+            let newItems = props.elements;
+            if (selectedKeys instanceof Set) {
+              newItems = props.elements.filter(
+                item => !selectedKeys.has(item.key)
+              );
+            } else if (selectedKeys === 'all') {
+              newItems = [];
+            }
+            props.onChange(newItems);
+            setSelectedKeys(new Set());
+          }
+        }}
+      >
+        <Item key="delete" textValue="Remove">
+          <Icon src={trash2Icon} />
+          <Text>Remove</Text>
+        </Item>
+      </ActionBar>
+    </ActionBarContainer>
+  );
+}
+
+function arrayFieldEmptyState() {
+  return (
+    <VStack
+      gap="large"
+      alignItems="center"
+      justifyContent="center"
+      height="100%"
+      padding="regular"
+    >
+      {/* <Text
+        elementType="h3"
+        align="center"
+        color="neutralSecondary"
+        size="large"
+        weight="medium"
+      >
+        Empty list
+      </Text> */}
+      <Text align="center" color="neutralTertiary">
+        No items selected…
+      </Text>
+    </VStack>
+  );
+}
