7ffd4a375fb133eaf68c941cbaeeb65f081006c6
Simon Vrachliotis <simon@simonswiss.com>
2024-08-14 10:28:09 +1000

    Newsletter thinkmill opt-in checkboxes + fix light button spinner

--- docs/src/components/forms/mailing-list.tsx
+++ docs/src/components/forms/mailing-list.tsx
@@ -27,6 +27,46 @@ export default function MailingListForm() {
           className="form-input h-8 w-full rounded-md border border-sand-6 bg-sand-1 px-3 py-0 text-sm leading-none hover:border-sand-8"
         />
       </div>
+      <div className="flex flex-wrap gap-x-6 gap-y-2">
+        <div className="flex items-center gap-2">
+          <input
+            type="checkbox"
+            name="keystatic_list"
+            id="mailing-list-keystatic"
+            className="form-checkbox size-4 rounded text-black"
+            checked
+          />
+          <label
+            className="text-sm text-sand-11"
+            htmlFor="mailing-list-keystatic"
+          >
+            Keystatic news
+          </label>
+        </div>
+        <div className="flex items-center gap-2">
+          <input
+            type="checkbox"
+            name="thinkmill_list"
+            id="mailing-list-thinkmill"
+            className="form-checkbox size-4 rounded text-black"
+          />
+          <label
+            className="text-sm text-sand-11"
+            htmlFor="mailing-list-thinkmill"
+          >
+            Thinkmill news (
+            <a
+              href="https://www.thinkmill.com.au/newsletter"
+              target="_blank"
+              className="cursor-pointer underline hover:text-thinkmill-red"
+              aria-label="Thinkmill (Opens in new tab)"
+            >
+              example
+            </a>
+            )
+          </label>
+        </div>
+      </div>
       <Button
         className="self-start"
         type="submit"
