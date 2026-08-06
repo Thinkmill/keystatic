2d3555fe7e3261bddb07f888cd45cbee57f02589
Emma Hamilton <git@emmas.town>
2026-08-06 12:52:00 +1000

    Compose picker fields with React Aria Components

--- design-system/pkg/src/combobox/ComboboxBase.tsx
+++ design-system/pkg/src/combobox/ComboboxBase.tsx
@@ -9,13 +9,24 @@ import type { ForwardedRef } from 'react';
 
 import { FieldButton } from '@keystar/ui/button';
 import type { FieldButtonProps } from '@keystar/ui/button';
-import { FieldPrimitive } from '@keystar/ui/field';
 import { Icon } from '@keystar/ui/icon';
 import { chevronsUpDownIcon } from '@keystar/ui/icon/icons/chevronsUpDownIcon';
 import { ListBox } from '@keystar/ui/listbox';
-import { css, tokenSchema } from '@keystar/ui/style';
+import {
+  classNames,
+  css,
+  filterStyleProps,
+  tokenSchema,
+  useStyleProps,
+} from '@keystar/ui/style';
 
 import type { ComboboxMultiProps, ComboboxProps } from './types';
+import {
+  FieldDescriptionElement,
+  FieldErrorElement,
+  FieldLabelElement,
+  fieldRootClassName,
+} from '../field/FieldElements';
 
 export function ComboboxBase<T extends object, M extends 'single' | 'multiple'>(
   props: M extends 'multiple' ? ComboboxMultiProps<T> : ComboboxProps<T>,
@@ -25,84 +36,92 @@ export function ComboboxBase<T extends object, M extends 'single' | 'multiple'>(
   let {
     align = 'start',
     children,
+    contextualHelp,
     dependencies,
+    description,
     direction = 'bottom',
     label,
     menuWidth,
+    errorMessage,
+    isRequired,
+    labelElementType: _labelElementType,
     shouldFlip = true,
     ...comboBoxProps
   } = props;
+  let styleProps = useStyleProps({ width: 'alias.singleLineWidth', ...props });
 
   return (
     <AriaComboBox
-      {...(comboBoxProps as AriaComboBoxProps<T, M>)}
-      aria-label={
-        comboBoxProps['aria-label'] ??
-        (typeof label === 'string' ? label : undefined)
-      }
+      {...(filterStyleProps(comboBoxProps) as AriaComboBoxProps<T, M>)}
       ref={forwardedRef}
       selectionMode={selectionMode}
+      isInvalid={comboBoxProps.isInvalid || Boolean(errorMessage)}
+      isRequired={isRequired}
+      className={classNames(fieldRootClassName, styleProps.className)}
+      style={styleProps.style}
     >
-      <FieldPrimitive width="alias.singleLineWidth" {...props}>
-        <>
-          <div
-            className={css({
-              alignItems: 'center',
-              display: 'flex',
-              position: 'relative',
-              width: '100%',
-            })}
-          >
-            <Input
-              name={comboBoxProps.name}
-              placeholder={props.placeholder}
-              className={css({
-                backgroundColor: tokenSchema.color.background.canvas,
-                border: `${tokenSchema.size.border.regular} solid ${tokenSchema.color.alias.borderIdle}`,
-                borderRadius: tokenSchema.size.radius.regular,
-                boxSizing: 'border-box',
-                color: tokenSchema.color.alias.foregroundIdle,
-                font: 'inherit',
-                height: tokenSchema.size.element.regular,
-                outline: 0,
-                paddingInline: tokenSchema.size.space.medium,
-                paddingInlineEnd: tokenSchema.size.element.regular,
-                width: '100%',
-                '&[data-focused]': {
-                  borderColor: tokenSchema.color.alias.borderFocused,
-                  boxShadow: `0 0 0 1px ${tokenSchema.color.alias.borderFocused}`,
-                },
-                '&[data-disabled]': {
-                  backgroundColor:
-                    tokenSchema.color.background.surfaceSecondary,
-                  color: tokenSchema.color.alias.foregroundDisabled,
-                },
+      <FieldLabelElement
+        contextualHelp={contextualHelp}
+        isRequired={isRequired}
+        label={label}
+      />
+      <FieldDescriptionElement>{description}</FieldDescriptionElement>
+      <div
+        className={css({
+          alignItems: 'center',
+          display: 'flex',
+          position: 'relative',
+          width: '100%',
+        })}
+      >
+        <Input
+          name={comboBoxProps.name}
+          placeholder={props.placeholder}
+          className={css({
+            backgroundColor: tokenSchema.color.background.canvas,
+            border: `${tokenSchema.size.border.regular} solid ${tokenSchema.color.alias.borderIdle}`,
+            borderRadius: tokenSchema.size.radius.regular,
+            boxSizing: 'border-box',
+            color: tokenSchema.color.alias.foregroundIdle,
+            font: 'inherit',
+            height: tokenSchema.size.element.regular,
+            outline: 0,
+            paddingInline: tokenSchema.size.space.medium,
+            paddingInlineEnd: tokenSchema.size.element.regular,
+            width: '100%',
+            '&[data-focused]': {
+              borderColor: tokenSchema.color.alias.borderFocused,
+              boxShadow: `0 0 0 1px ${tokenSchema.color.alias.borderFocused}`,
+            },
+            '&[data-disabled]': {
+              backgroundColor: tokenSchema.color.background.surfaceSecondary,
+              color: tokenSchema.color.alias.foregroundDisabled,
+            },
+          })}
+        />
+        <AriaButton
+          render={({ className: _, ...buttonProps }) => (
+            <FieldButton
+              {...(buttonProps as FieldButtonProps)}
+              prominence="low"
+              UNSAFE_className={css({
+                insetInlineEnd: 0,
+                position: 'absolute',
               })}
             />
-            <AriaButton
-              render={({ className: _, ...buttonProps }) => (
-                <FieldButton
-                  {...(buttonProps as FieldButtonProps)}
-                  prominence="low"
-                  UNSAFE_className={css({
-                    insetInlineEnd: 0,
-                    position: 'absolute',
-                  })}
-                />
-              )}
-            >
-              <Icon src={chevronsUpDownIcon} />
-            </AriaButton>
-          </div>
-          <Popover
-            placement={`${direction} ${align}`}
-            shouldFlip={shouldFlip}
-            style={{ minWidth: 'var(--trigger-width)', width: menuWidth }}
-          >
-            <ListBox dependencies={dependencies}>{children}</ListBox>
-          </Popover>
-        </>
-      </FieldPrimitive>
+          )}
+        >
+          <Icon src={chevronsUpDownIcon} />
+        </AriaButton>
+      </div>
+      <Popover
+        placement={`${direction} ${align}`}
+        shouldFlip={shouldFlip}
+        style={{ minWidth: 'var(--trigger-width)', width: menuWidth }}
+      >
+        <ListBox dependencies={dependencies}>{children}</ListBox>
+      </Popover>
+      <FieldErrorElement>{errorMessage}</FieldErrorElement>
     </AriaComboBox>
   );
 }
--- design-system/pkg/src/picker/Picker.tsx
+++ design-system/pkg/src/picker/Picker.tsx
@@ -2,6 +2,7 @@ import { Button as AriaButton } from 'react-aria-components/Button';
 import { Popover as AriaPopover } from 'react-aria-components/Popover';
 import {
   Select as AriaSelect,
+  type SelectProps as AriaSelectProps,
   SelectValue,
 } from 'react-aria-components/Select';
 import { type ForwardedRef, type ReactElement, forwardRef } from 'react';
@@ -9,16 +10,27 @@ import { useLocalizedStringFormatter } from 'react-aria/useLocalizedStringFormat
 
 import { FieldButton } from '@keystar/ui/button';
 import type { FieldButtonProps } from '@keystar/ui/button';
-import { FieldPrimitive } from '@keystar/ui/field';
 import { Icon } from '@keystar/ui/icon';
 import { chevronsUpDownIcon } from '@keystar/ui/icon/icons/chevronsUpDownIcon';
 import { ListBox } from '@keystar/ui/listbox';
 import { SlotProvider } from '@keystar/ui/slots';
-import { css, tokenSchema } from '@keystar/ui/style';
+import {
+  classNames,
+  css,
+  filterStyleProps,
+  tokenSchema,
+  useStyleProps,
+} from '@keystar/ui/style';
 import { Text } from '@keystar/ui/typography';
 
 import type { PickerProps } from './types';
 import localizedMessages from './l10n';
+import {
+  FieldDescriptionElement,
+  FieldErrorElement,
+  FieldLabelElement,
+  fieldRootClassName,
+} from '../field/FieldElements';
 
 function Picker<T extends object>(
   props: PickerProps<T>,
@@ -28,79 +40,89 @@ function Picker<T extends object>(
   let {
     align = 'start',
     children,
+    contextualHelp,
     dependencies,
+    description,
     direction = 'bottom',
     items,
     label,
     menuWidth,
+    errorMessage,
+    isRequired,
+    labelElementType: _labelElementType,
     placeholder = stringFormatter.format('placeholder'),
     prominence,
     shouldFlip = true,
     ...selectProps
   } = props;
+  let styleProps = useStyleProps({ width: 'alias.singleLineWidth', ...props });
 
   return (
     <AriaSelect
-      {...selectProps}
-      aria-label={
-        selectProps['aria-label'] ??
-        (typeof label === 'string' ? label : undefined)
-      }
+      {...(filterStyleProps(selectProps) as AriaSelectProps<T>)}
       ref={forwardedRef}
+      isInvalid={selectProps.isInvalid || Boolean(errorMessage)}
+      isRequired={isRequired}
+      className={classNames(fieldRootClassName, styleProps.className)}
+      style={styleProps.style}
     >
-      <FieldPrimitive width="alias.singleLineWidth" {...props}>
-        <>
-          <AriaButton
-            render={({ className: _, ...buttonProps }) => (
-              <FieldButton
-                {...(buttonProps as FieldButtonProps)}
-                autoFocus={props.autoFocus}
-                isDisabled={props.isDisabled}
-                prominence={prominence}
-                UNSAFE_className={css({
-                  alignItems: 'center',
-                  contain: 'size',
-                  display: 'flex',
-                  justifyContent: 'space-between',
-                  position: 'relative',
-                  width: '100%',
-                })}
-              />
+      <FieldLabelElement
+        contextualHelp={contextualHelp}
+        isRequired={isRequired}
+        label={label}
+        supplementRequiredState
+      />
+      <FieldDescriptionElement>{description}</FieldDescriptionElement>
+      <AriaButton
+        render={({ className: _, ...buttonProps }) => (
+          <FieldButton
+            {...(buttonProps as FieldButtonProps)}
+            autoFocus={props.autoFocus}
+            isDisabled={props.isDisabled}
+            prominence={prominence}
+            UNSAFE_className={css({
+              alignItems: 'center',
+              contain: 'size',
+              display: 'flex',
+              justifyContent: 'space-between',
+              position: 'relative',
+              width: '100%',
+            })}
+          />
+        )}
+      >
+        <SlotProvider
+          slots={{
+            icon: { marginEnd: 'small' },
+            text: { color: 'inherit' },
+            description: { isHidden: true },
+          }}
+        >
+          <SelectValue>
+            {({ isPlaceholder, selectedText }) => (
+              <Text color={isPlaceholder ? 'neutralSecondary' : 'inherit'}>
+                {selectedText || placeholder}
+              </Text>
             )}
-          >
-            <SlotProvider
-              slots={{
-                icon: { marginEnd: 'small' },
-                text: { color: 'inherit' },
-                description: { isHidden: true },
-              }}
-            >
-              <SelectValue>
-                {({ isPlaceholder, selectedText }) => (
-                  <Text color={isPlaceholder ? 'neutralSecondary' : 'inherit'}>
-                    {selectedText || placeholder}
-                  </Text>
-                )}
-              </SelectValue>
-            </SlotProvider>
-            <Icon
-              src={chevronsUpDownIcon}
-              UNSAFE_className={css({
-                marginInlineStart: tokenSchema.size.space.small,
-              })}
-            />
-          </AriaButton>
-          <AriaPopover
-            placement={`${direction} ${align}`}
-            shouldFlip={shouldFlip}
-            style={{ minWidth: 'var(--trigger-width)', width: menuWidth }}
-          >
-            <ListBox items={items} dependencies={dependencies}>
-              {children}
-            </ListBox>
-          </AriaPopover>
-        </>
-      </FieldPrimitive>
+          </SelectValue>
+        </SlotProvider>
+        <Icon
+          src={chevronsUpDownIcon}
+          UNSAFE_className={css({
+            marginInlineStart: tokenSchema.size.space.small,
+          })}
+        />
+      </AriaButton>
+      <AriaPopover
+        placement={`${direction} ${align}`}
+        shouldFlip={shouldFlip}
+        style={{ minWidth: 'var(--trigger-width)', width: menuWidth }}
+      >
+        <ListBox items={items} dependencies={dependencies}>
+          {children}
+        </ListBox>
+      </AriaPopover>
+      <FieldErrorElement>{errorMessage}</FieldErrorElement>
     </AriaSelect>
   );
 }
